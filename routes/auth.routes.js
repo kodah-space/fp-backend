45f5e0fccd356d72c3873a987ac20f205e471866
Mira Witte <mira.witte@gmx.de>
2024-05-29 11:56:07 +0200

    Tue1

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -147,7 +147,7 @@ router.post("/login", (req, res, next) => {
         });
 
         // Send the token as the response
-        res.status(200).json({ authToken: authToken });
+        res.status(200).json({ authToken, user: payload });
       } else {
         res.status(401).json({ message: "Unable to authenticate the user" });
       }
