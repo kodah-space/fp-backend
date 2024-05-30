b37006936a09ba9e30510258876e8226d2386048
Shakthi Prasad KT <shakthi.kt@gmail.com>
2024-05-30 13:37:20 +0200

    fixed verify to update user info

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -161,8 +161,26 @@ router.get("/verify", isAuthenticated, (req, res, next) => {
   // isAuthenticated middleware and is made available on `req.payload`
   console.log(`req.payload`, req.payload);
 
-  // Send back the token payload object containing the user data
-  res.status(200).json(req.payload);
+  const userId = req.payload._id;
+
+  // Fetch the updated user info from the database
+  User.findById(userId)
+    .then((foundUser) => {
+      if (!foundUser) {
+        res.status(404).json({ message: "User not found." });
+        return;
+      }
+
+      // Deconstruct the user object to omit the password
+      const { userName, email, name, dateOfBirth, _id, profilePic } = foundUser;
+
+      // Create an object that doesn't expose the password
+      const user = { userName, email, name, dateOfBirth, _id, profilePic };
+
+      // Send back the user data
+      res.status(200).json(user);
+    })
+    .catch((err) => next(err)); // In this case, we send error handling to the error handling middleware.
 });
 
 // PUT /auth/update - Updates the user profile
@@ -231,7 +249,9 @@ router.put("/update", isAuthenticated, (req, res, next) => {
       const { userName, email, name, dateOfBirth, _id, profilePic } =
         updatedUser;
       const user = { userName, email, name, dateOfBirth, _id, profilePic };
-
+      req.payload = user;
+      console.log("updated user:", user);
+      return;
       res.status(200).json({ user });
     })
     .catch((err) => next(err));
